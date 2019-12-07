a1ff3432e72ced050fe50741e65de63483e69d31
Ricky Wilson <echoquote@gmail.com>
2019-12-07 21:31:10 +0000

    pep8

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 This module is a demonstration of how to send
 a HTTP request from scratch with the socket module.
 """
+import socket
 
 __author__ = "Ricky L Wilson."
 __email__ = "echoquote@gmail.com"
-
-import socket
-
 
 """
 The term CRLF refers to Carriage Return (ASCII 13, \r)
@@ -27,9 +25,9 @@ def request_header(host=HOST, path=PATH):
     Create the request header.
     """
     return CRLF.join([
-      "GET {} HTTP/1.1".format(path),
-      "Host: {}".format(host),
-      "Connection: Close\r\n\r\n"])
+        "GET {} HTTP/1.1".format(path), "Host: {}".format(host),
+        "Connection: Close\r\n\r\n"
+    ])
 
 
 def send_request(host=HOST, path=PATH, port=PORT):
@@ -55,6 +53,6 @@ def send_request(host=HOST, path=PATH, port=PORT):
 
     # HTTP headers will be separated from the body by an empty line
     return response.partition(CRLF + CRLF)
-    
+
 
 print(send_request(host="www.google.com")[0])
